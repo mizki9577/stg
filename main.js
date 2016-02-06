91159df8b49d0b49e7be9c57966d256c295ff842
Mizki SUZUMORI <mizki9577@gmail.com>
2016-02-06 22:28:16 +0900

    Game オブジェクトをグローバルに置く

--- main.js
+++ main.js
@@ -3,8 +3,7 @@
 import Game from './game.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('Script Loaded');
-  let game = new Game(document.getElementById('canvas'));
+  window.Game = new Game(document.getElementById('canvas'));
 });
 
 // vim: set ts=2 sw=2 et:
