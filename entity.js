cc4738fcc06935f98c2c3c8c8742c859f41c22fb
Mizki SUZUMORI <mizki9577@gmail.com>
2016-03-17 05:38:36 +0900

    エンティティのフィールド上における座標に変換したパスを返す pathsInField を追加

--- entity.js
+++ entity.js
@@ -36,23 +36,22 @@ class Entity {
     }
   }
 
+  *pathsInField() {
+    const sin = Math.sin(-this.angle);
+    const cos = Math.cos(-this.angle);
+    for (const [[ax0, ay0], [bx0, by0]] of this.paths) {
+      const ax = (ax0 * cos - ay0 * sin) + this.x;
+      const ay = (ax0 * sin + ay0 * cos) + this.y;
+      const bx = (bx0 * cos - by0 * sin) + this.x;
+      const by = (bx0 * sin + by0 * cos) + this.y;
+      yield [[ax, ay], [bx, by]];
+    }
+  }
+
   static areTheyCollided(entityA, entityB) {
-    for (const [[a1x0, a1y0], [a2x0, a2y0]] of entityA.paths) {
-      const sinA = Math.sin(-entityA.angle);
-      const cosA = Math.cos(-entityA.angle);
-      const a1x = (a1x0 * cosA - a1y0 * sinA) + entityA.x;
-      const a1y = (a1x0 * sinA + a1y0 * cosA) + entityA.y;
-      const a2x = (a2x0 * cosA - a2y0 * sinA) + entityA.x;
-      const a2y = (a2x0 * sinA + a2y0 * cosA) + entityA.y;
-
-      for (const [[b1x0, b1y0], [b2x0, b2y0]] of entityB.paths) {
-        const sinB = Math.sin(-entityB.angle);
-        const cosB = Math.cos(-entityB.angle);
-        const b1x = (b1x0 * cosB - b1y0 * sinB) + entityB.x;
-        const b1y = (b1x0 * sinB + b1y0 * cosB) + entityB.y;
-        const b2x = (b2x0 * cosB - b2y0 * sinB) + entityB.x;
-        const b2y = (b2x0 * sinB + b2y0 * cosB) + entityB.y;
-
+    const ctx = Game.ctx;
+    for (const [[a1x, a1y], [a2x, a2y]] of entityA.pathsInField()) {
+      for (const [[b1x, b1y], [b2x, b2y]] of entityB.pathsInField()) {
         const ta = (b1x - b2x) * (a1y - b1y) + (b1y - b2y) * (b1x - a1x);
         const tb = (b1x - b2x) * (a2y - b1y) + (b1y - b2y) * (b1x - a2x);
         const tc = (a1x - a2x) * (b1y - a1y) + (a1y - a2y) * (a1x - b1x);
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import 'babel-polyfill';
 import Game from './game.js';
 
 window.addEventListener('DOMContentLoaded', () => {
